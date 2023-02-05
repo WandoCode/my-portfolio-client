5543402cd2cd4c7b36b0b9bab0a8dee52857f002
Wandole <mchirez@gmail.com>
2023-02-05 22:28:38 +0100

    sync deploy

--- components/Home/Skills/SkillItem.tsx
+++ components/Home/Skills/SkillItem.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image, { ImageLoader, ImageLoaderProps } from 'next/image'
 import { isInteger } from '../../../utils/helpers/number'
 import { SkillDatas } from './Skills'
 import { useEffect, useState } from 'react'
@@ -32,11 +32,21 @@ function SkillItem({ datas }: Props) {
     return rating
   }
 
+  const myLoader = ({ src, width, quality }: ImageLoaderProps) => {
+    return `${src}`
+  }
+
   return (
     <li className="skill">
       {ratingNumber && <div className="skill__rating">{ratingDOM()}</div>}
       <div className="skill__img-container">
-        <Image src={datas.urlIcon} height={50} width={50} alt="" />
+        <Image
+          loader={myLoader}
+          src={datas.urlIcon}
+          height={50}
+          width={50}
+          alt=""
+        />
       </div>
       <h3 className="h3 skill__title">
         <span className="skill__title-text">{datas.title}</span>
