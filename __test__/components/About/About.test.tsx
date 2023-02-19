dc40f7c3ed4d04cfce1d6a11232e880c4bacb90b
Wandole <mchirez@gmail.com>
2023-02-19 17:03:39 +0100

    Rewrite About tests

--- __test__/components/About/About.test.tsx
+++ __test__/components/About/About.test.tsx
@@ -1,19 +1,14 @@
 import { describe, test, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import About from '../../../components/Home/About/About'
+import About from '../../../components/HomeSections/AboutSection/About'
 import mockedMainDatas from '../../__mock__/datas/mockedMainDatas.json'
-import MockLanguageProvider from '../../__mock__/Language/MockLanguageProvider'
 
 const aboutDatas = mockedMainDatas.about
 
 describe('Given the About component is used', () => {
   describe('When the "fr" language is selected', () => {
     test('It should display datas from the french part of the datas', async () => {
-      render(
-        <MockLanguageProvider lang="fr">
-          <About aboutDatas={aboutDatas} />
-        </MockLanguageProvider>
-      )
+      render(<About aboutDatas={aboutDatas} language="fr" />)
       const textA = await screen.findByText(aboutDatas.text1.fr)
       const textB = await screen.findByText(aboutDatas.text2.fr)
 
@@ -21,14 +16,19 @@ describe('Given the About component is used', () => {
       expect(textB).toBeDefined()
     })
   })
+  describe('When the "es" language is selected', () => {
+    test('It should display datas from the spanish part of the datas', async () => {
+      render(<About aboutDatas={aboutDatas} language="es" />)
+      const textA = await screen.findByText(aboutDatas.text1.es)
+      const textB = await screen.findByText(aboutDatas.text2.es)
 
+      expect(textA).toBeDefined()
+      expect(textB).toBeDefined()
+    })
+  })
   describe('When the "en" language is selected', () => {
     test('It should display datas from the english part of the datas', async () => {
-      render(
-        <MockLanguageProvider lang="en">
-          <About aboutDatas={aboutDatas} />
-        </MockLanguageProvider>
-      )
+      render(<About aboutDatas={aboutDatas} language="en" />)
       const textA = await screen.findByText(aboutDatas.text1.en)
       const textB = await screen.findByText(aboutDatas.text2.en)
 
@@ -37,18 +37,13 @@ describe('Given the About component is used', () => {
     })
   })
 
-  describe('When the "es" language is selected', () => {
-    test('It should display datas from the spanish part of the datas', async () => {
-      render(
-        <MockLanguageProvider lang="es">
-          <About aboutDatas={aboutDatas} />
-        </MockLanguageProvider>
-      )
-      const textA = await screen.findByText(aboutDatas.text1.es)
-      const textB = await screen.findByText(aboutDatas.text2.es)
+  test('It should display an image with a given alt text', async () => {
+    render(<About aboutDatas={aboutDatas} language="fr" />)
+    const image: HTMLImageElement = await screen.findByAltText(
+      aboutDatas.img.alt
+    )
 
-      expect(textA).toBeDefined()
-      expect(textB).toBeDefined()
-    })
+    expect(image).toBeDefined()
+    expect(image.src).toMatch(aboutDatas.img.link.split('/')[1])
   })
 })
