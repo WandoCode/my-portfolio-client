13189ba378a3969304bc71fe914d95c31cae11c3
Wandole <mchirez@gmail.com>
2023-02-19 21:27:47 +0100

    Add a function to transforme rgb color into hex color

--- utils/helpers/string.ts
+++ utils/helpers/string.ts
@@ -3,3 +3,14 @@ export const isValidEmail = (str: string) => {
   const matches = str.matchAll(emailRegEx)
   return Array.from(matches).length === 1
 }
+
+export /* Source: https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb/5623914#5623914 */
+function rgbToHex(rgbStr: string) {
+  const rgbSplited = rgbStr.split(',')
+
+  const r = +rgbSplited[0].split('(')[1].trim()
+  const g = +rgbSplited[1].trim()
+  const b = +rgbSplited[2].split(')')[0]
+
+  return '#' + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1)
+}
