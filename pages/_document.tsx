c62c976162ef3d9dbc315f2e6045324ab186da08
Wandole <mchirez@gmail.com>
2023-02-21 19:12:16 +0100

    Cleaned assets

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,30 @@ function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/assets/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/assets/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         {/* Allow to avoid a flash on screen between light and dark theme at page loading.
