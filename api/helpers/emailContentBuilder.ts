dab6d3aa14d896aaa1667d0fe6ad25f80ac40687
Wandole <mchirez@gmail.com>
2023-02-21 18:22:52 +0100

    Improve email style and content

--- api/helpers/emailContentBuilder.ts
+++ api/helpers/emailContentBuilder.ts
@@ -9,68 +9,68 @@ const buildEmail = (
 
   switch (lang) {
     case 'fr':
-      return `<h1>Confirmation de réception</h1>
+      return `
               <p>Bonjour ${messageDatas.name},</p>
               <p>J'ai bien reçu votre message et vous répondrez dés que possible.</p>
-              <h2>Détails du message:</h2>
-              <ul>
+              <p style="padding-top: 1em; font-size: 1.15em; font-weight:500;">Détails du message</p>
+              <ul style="padding: 0;list-style: none;">
                 <li><em>De</em>: ${messageDatas.name}</li>
                 <li><em>Objet</em>: ${messageDatas.object}</li>
                 <li><em>Email</em>: ${messageDatas.email}</li>
                 <li><em>Message</em>: 
                 <br>
-                <div style="background-color: hsl(0, 0%, 90%); padding: 3rem;">${messageHTML}</div>
+                <p style="background-color: hsl(0, 0%, 95%); padding: 1em 1.5em; border-radius: 8px; margin-top: 0.75em;">${messageHTML}</p>
                  <br> 
                  </li>
               </ul>
               <p>Maxime Chirez, développeur frontend </p>`
     case 'es':
-      return `<h1>Confirmacion de recepcion</h1>
+      return `
               <p>Hola ${messageDatas.name},</p>
               <p>Le confirmo la reception de su mensaje, le contestaré cuanto antes.</p>
-              <h2>Detalles del mensaje:</h2>
-              <ul>
+              <p style="padding-top: 1em; font-size: 1.15em; font-weight:500;">Detalles del mensaje</p>
+              <ul style="padding: 0;list-style: none;">
                 <li><em>De</em>: ${messageDatas.name}</li>
                 <li><em>Objeto</em>: ${messageDatas.object}</li>
                 <li><em>Correo</em>: ${messageDatas.email}</li>
                 <li><em>Mensaje</em>: 
                 <br>
-                <div style="background-color: hsl(0, 0%, 90%); padding: 3rem;">${messageHTML}</div>
+                <p style="background-color: hsl(0, 0%, 95%); padding: 1em 1.5em; border-radius: 8px; margin-top: 0.75em;">${messageHTML}</p>
                  <br> 
                  </li>
               </ul>
               <p>Maxime Chirez, frontend programdor (????) </p>`
     case 'en':
-      return `<h1>Reception confirmation</h1>
+      return `
               <p>Hello ${messageDatas.name},</p>
               <p>I received your message and I will answer you as soon as possible.</p>
-              <h2>Message details:</h2>
-              <ul>
+              <p style="padding-top: 1em; font-size: 1.15em; font-weight:500;">Message details</p>
+              <ul style="padding: 0;list-style: none;">
                 <li><em>From</em>: ${messageDatas.name}</li>
                 <li><em>Object</em>: ${messageDatas.object}</li>
                 <li><em>Email</em>: ${messageDatas.email}</li>
                 <li><em>Message</em>: 
                 <br>
-                <div style="background-color: hsl(0, 0%, 90%); padding: 3rem;">${messageHTML}</div>
+                <p style="background-color: hsl(0, 0%, 95%); padding: 1em 1.5em; border-radius: 8px; margin-top: 0.75em;">${messageHTML}</p>
                  <br> 
                  </li>
               </ul>
               <p>Maxime Chirez, frontend developer</p>`
     // TODO: Vérifier la version EN, et surtout la version ES!!!!!
     default:
-      return `<h1>Reception confirmation</h1>
+      return `
               <p>Hello ${messageDatas.name},</p>
-              <p>You email has been send and I will answer as soon as possible.</p>
-              <h2>Message details:</h2>
-              <ul>
-                <li>From: ${messageDatas.name}</li>
-                <li>Object: ${messageDatas.object}</li>
-                <li>Email: ${messageDatas.email}</li>
+              <p>I received your message and I will answer you as soon as possible.</p>
+              <p style="padding-top: 1em; font-size: 1.15em; font-weight:500;">Message details</p>
+              <ul style="padding: 0;list-style: none;">
+                <li><em>From</em>: ${messageDatas.name}</li>
+                <li><em>Object</em>: ${messageDatas.object}</li>
+                <li><em>Email</em>: ${messageDatas.email}</li>
                 <li><em>Message</em>: 
                 <br>
-                <div style="background-color: hsl(0, 0%, 90%); padding: 3rem;">${messageHTML}</div>
-                 <br> 
-                 </li>
+                <p style="background-color: hsl(0, 0%, 95%); padding: 1em 1.5em; border-radius: 8px; margin-top: 0.75em;">${messageHTML}</p>
+                <br> 
+                </li>
               </ul>
               <p>Maxime Chirez, frontend developer</p>`
   }
@@ -79,15 +79,15 @@ const buildEmail = (
 const buildObject = (lang: LanguageAvailable | null) => {
   switch (lang) {
     case 'fr':
-      return 'Votre message a bien été envoyé!'
+      return 'Confirmation de réception'
 
     case 'en':
-      return 'Your message has been send successfully!'
+      return 'Reception confirmation'
     case 'es':
-      return 'Su mensaje ha sido enviado con exito!'
+      return 'Confirmacion de recepcion'
 
     default:
-      return 'Your message has been send successfully!'
+      return 'Reception confirmation'
   }
 }
 export { buildEmail, buildObject }
